020953b8caa2f9b94a825acdea68f2e7693e8ed4
Rakbardzo <114349045+Rakbardzo@users.noreply.github.com>
2022-09-27 19:42:14 +0200

    Update idlemarket.js

--- idlemarket.js
+++ idlemarket.js
@@ -17,7 +17,7 @@ IdleTrading.launch = function(){
 			conf.goods.push({
 				active: true,
 				buyThresh: 10,
-				sellThresh: 110,
+				sellThresh: 130,
 				minPrice: 99999,
 				maxPrice:-99999
 			});
